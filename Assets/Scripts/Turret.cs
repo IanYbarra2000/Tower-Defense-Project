970df000bd24455266c8c9e65fe096ee69cc90b5
Ian Ybarra <50596097+IanYbarra2000@users.noreply.github.com>
2021-10-15 15:50:19 -0500

    Add place holder Temoc sprite and turret rotation

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range = 10f;
+
+    public string enemyTag = "Enemy";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f,.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach( GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+            if(nearestEnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+        
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle+90f, Vector3.forward);/*
+        Quaternion lookRotation = Quaternion.LookRotation(dir,transform.TransformDirection(Vector3.up))*Quaternion.Euler(0,0,-270f);
+        transform.rotation = new Quaternion(0, 0, lookRotation.z, lookRotation.w);
+        Vector3 rotation = lookRotation.eulerAngles;
+        Debug.Log(rotation);
+        transform.rotation = Quaternion.Euler(rotation.x,0f,0f);*/
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
