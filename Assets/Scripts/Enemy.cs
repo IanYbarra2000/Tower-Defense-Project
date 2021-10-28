b885388df1b772545eb2bae801001ec750ad5512
Ian Ybarra <50596097+IanYbarra2000@users.noreply.github.com>
2021-10-28 16:17:13 -0500

    Add new art and animation for basic enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,6 +22,8 @@ public class Enemy : MonoBehaviour
         Vector2 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle+90f, Vector3.forward);
         if (Vector2.Distance(transform.position, target.position) <= .1f)
         {
             GetNextWaypoint();
