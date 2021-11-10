d21cc504d96b8eaaa0f258eba388cb9562840a3a
Ian Ybarra <50596097+IanYbarra2000@users.noreply.github.com>
2021-11-10 15:03:10 -0600

    Add two new enemy types and chage wave spawner

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform enemyPrefab;
+    public Transform[] enemyPrefabs;
 
     [SerializeField] public Transform spawnPoint;
     [SerializeField] public float timeBetweenWaves = 5f;
     [SerializeField] public float timeBetweenEnemies = 0.2f; 
     [SerializeField] public float countdown = 2f;
 
-    private int waveIndex = 0;
+    private int waveIndex = 0; //acts as a count for which wave is happening and a count for the amount of total enemy health allowed for the wave
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,16 +32,35 @@ public class WaveSpawner : MonoBehaviour
     IEnumerator SpawnWave()
     {
         waveIndex++;
+        print("Spawn wave "+waveIndex); 
         for (int i = 0; i < waveIndex; i++)
         {
-            SpawnEnemy();
+            int spawnIndex = chooseEnemy(waveIndex-i);//chooses which enemy to spawn
+            SpawnEnemy(spawnIndex);
+            i += enemyPrefabs[spawnIndex].gameObject.GetComponent<Enemy>().health-1;
             yield return new WaitForSeconds(timeBetweenEnemies);
         }
         
     }
 
-    void SpawnEnemy()
+    int chooseEnemy(int healthLeft){
+        /* 
+        Chooses random enemy to spawn so long as it it doesn't
+        have more health than the amount that is allowed during
+        a particular wave
+        */
+
+        int start = Random.Range(0,enemyPrefabs.Length);
+        print("random num ="+start);
+        for (int i = start;i>=0;i--){
+            if(enemyPrefabs[i].gameObject.GetComponent<Enemy>().health <= healthLeft){
+                return i;
+            }
+        }
+        return 0;
+    }
+    void SpawnEnemy(int index)
     {
-        Instantiate(enemyPrefab, spawnPoint.position,spawnPoint.rotation);
+        Instantiate(enemyPrefabs[index], spawnPoint.position,spawnPoint.rotation);
     }
 }
